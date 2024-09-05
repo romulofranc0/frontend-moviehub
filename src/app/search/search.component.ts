884f7b791d03c8bb5ea77d12a576a90a0f32c302
Romulo Maciel <romulo.maciel@wmtrading.com.br>
2024-09-05 15:25:09 -0300

    implementing search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,32 @@
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { OmdbResponse } from '../models/OmdbResponse';
+import { MovieApi } from '../../services/Movie-api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [FormsModule, InputTextModule],
+  imports: [FormsModule, InputTextModule, ButtonModule, CommonModule],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
   title:string ='';
+  movies:OmdbResponse[] = [];
+  errorMessage: string = '';
 
+  constructor(private movieApi:MovieApi){}
+
+  searchMovies() {
+
+      this.movieApi.getMovies(this.title).subscribe({
+        next:(response) =>{
+          this.movies = response;
+        }
+      });
+      }
 }
+
