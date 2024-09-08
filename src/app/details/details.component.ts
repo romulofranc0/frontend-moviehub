9ed897c6fe8e187eddc86e41d06760d5d9c6c8cc
romulofranc0 <rfsvm97@gmail.com>
2024-09-08 12:29:51 -0300

    get movie details in Details Component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,14 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { SearchComponent } from '../search/search.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieApi } from '../../services/Movie-api.service';
+import { MovieDetails } from '../models/MovieDetails';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, ButtonModule, ProgressSpinnerModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
+  @Input() imdbID='' ;
+  details!: MovieDetails;
+  loading: boolean = false;
+
+  constructor(private route: ActivatedRoute, private movieApi:MovieApi){}
+  
+  ngOnInit() {
+    this.imdbID = this.route.snapshot.paramMap.get('imdbID') || '';
+    
+    this.movieApi.getMovieDetails(this.imdbID).subscribe({
+      next: (data: MovieDetails) => {
+        this.details = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.loading = false;
+        console.error('Erro ao obter detalhes do filme:', err);
+      }
+    });
+  }
 
 }
